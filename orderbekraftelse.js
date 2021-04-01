319c6ec29836393dc5db406da754523dd26b819c
Rakib <rakib121@gmail.com>
2021-04-01 12:47:17 +0200

    init2

--- orderbekraftelse.js
+++ orderbekraftelse.js
@@ -1,8 +1,10 @@
+let cartItems = localStorage.getItem("cart");
+cartItems = JSON.parse(cartItems);
+
 function orderBekraftelse() {
-    let cartItems = localStorage.getItem("cart");
-    cartItems = JSON.parse(cartItems);
     console.log(cartItems);
-    let productContainer = document.querySelector(".test2");
+    let productContainer = document.querySelector(".produkter");
+    let productTotal = document.querySelector(".totalt");
     if (cartItems && productContainer) {
         productContainer.innerHTML = '';
         Object.values(cartItems).map(item => {
@@ -10,14 +12,26 @@ function orderBekraftelse() {
             <tr>
             <td class="product-id">${item.id}</td>
                 <td class="product-namn">${item.title}</td>
-                <td class="product-price">${item.price} kr</td>
+                <td class="product-price">${(item.price).toFixed(2)} kr</td>
                 <td class="product-quantity">${item.quantity}</td>
-                <td class="product-weight">${item.weight} kr</td>     
-                <td class="product-total">${item.quantity * item.price} kr</td>     
+                <td class="product-weight">${(item.weight).toFixed(2)}</td>     
+                <td class="product-total">${(item.quantity * item.price).toFixed(2)} kr</td>          
             </tr>           
             `
         });
     }
 }
+
+let totalDiv = document.querySelector(".total");
+totalDiv.innerHTML = `Totalt: ` +getTotal();
+
+function getTotal() {
+    let sum = 0;
+    for (let i = 0; i < cartItems.length; i++) {
+        sum += parseFloat(cartItems[i].price) * parseInt(cartItems[i].quantity);
+    }
+    return sum;
+}
 orderBekraftelse();
+getTotal();
 
