74109ca0e4dd573b1c55105d429fc9425cc53a75
Sondaicus <54620152+Sondaicus@users.noreply.github.com>
2021-04-26 15:42:09 +0200

    2
    2

--- scripts/correctZIP.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('zipcode').addEventListener('input', function (e)
-{
-    var target = e.target, position = target.selectionEnd, length = target.value.length;
-    
-    target.value = target.value.replace(/^[a-öA-ö0-9]/g, '').replace(/(.{3})/g, '$1 ').trim();
-    target.selectionEnd = position += ((target.value.charAt(position - 1) === ' ' && target.value.charAt(length - 1) === ' ' && length !== target.value.length) ? 1 : 0);
-  });
